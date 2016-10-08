a67a448815767679d37f9d3c0393cf749344f960
Poh Zhi Ee <poh140996@hotmail.com>
2016-10-08 12:23:15 +0100

    printstatus

--- src/Day.cpp
+++ src/Day.cpp
@@ -21,15 +21,6 @@ void Day::printStatus_init(){
 	cout << "You have " << ration << " ration(s) of food. \n";
 	cout << "You have " << uncooked << " uncooked food. \n"
 	cout << sick << " member is sick. \n";
-=======
-void Day::printStatus(){
-	int member, ration, uncooked, weapon, sick, medicine, barricades;
-	cout << "The colony has " << member << " members. \n";
-	cout << "You have " << ration << " rations of food. \n";
-	cout << "You have " << uncooked << " uncooked food. \n";
-	cout << "You have " << weapon << " weapons. \n";
-	cout << sick << " members are sick. \n";
->>>>>>> origin/master
 	cout << "You have " << medicine << " medicine. \n";
 	cout << "You have " << barricades << " barricades. \n";
 }
