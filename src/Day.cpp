5e4e57943bd1dcb74c81976f2aeeba89542781e8
Poh Zhi Ee <poh140996@hotmail.com>
2016-10-08 11:26:05 +0100

    Print status

--- src/Day.cpp
+++ src/Day.cpp
@@ -4,11 +4,18 @@
  *  Created on: Oct 8, 2016
  *      Author: nichita
  */
-
+#include <iostream>
 #include "Day.h"
+#include "People.h"
+
+using namespace std;
 
 void Day::printStatus(){
-
+	int member, ration, weapon, sick, medicine;
+	cout << "The colony has " << member << " members. \n";
+	cout << "You have " << ration << " rations of food. \n";
+	cout << sick << " member is sick. \n";
+	cout << "You have " << medicine << " medicine. \n";
 }
 
 
