fbe7a3fcd4c35641bef6fe0b951a50bfe9f317e7
Poh Zhi Ee <poh140996@hotmail.com>
2016-10-08 11:45:38 +0100

    updated printstatus
    added day count

--- src/Day.cpp
+++ src/Day.cpp
@@ -7,15 +7,19 @@
 #include <iostream>
 #include "Day.h"
 #include "People.h"
+#include "Colony.h"
 
 using namespace std;
 
 void Day::printStatus(){
 	int member, ration, weapon, sick, medicine;
-	member = getPeople();
-	sick   = getSick();
-	cout << "The colony has " << member << " members. \n";
-	cout << "You have " << ration << " rations of food. \n";
+	
+	member = Colony.getPeople();
+	sick   = Colony.getSick();
+	cout << "DAY " << day_num << '\n';								 
+	cout << "--------------------------------------------------\n"   
+	cout << "The colony has " << member << " members. \n";			 
+	cout << "You have " << ration << " rations of food. \n";		 
 	cout << sick << " member is sick. \n";
 	cout << "You have " << medicine << " medicine. \n";
 }
