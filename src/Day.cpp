acfa051d6f69bda76b12e099643484d987299468
Jane <psysrc@nottingham.ac.uk>
2016-10-08 12:13:33 +0100

    Added the rest of the colony elements to the output messages.

--- src/Day.cpp
+++ src/Day.cpp
@@ -11,11 +11,14 @@
 using namespace std;
 
 void Day::printStatus(){
-	int member, ration, weapon, sick, medicine;
+	int member, ration, uncooked, weapon, sick, medicine, barricades;
 	cout << "The colony has " << member << " members. \n";
 	cout << "You have " << ration << " rations of food. \n";
+	cout << "You have " << uncooked << " uncooked food. \n";
+	cout << "You have " << weapon << " weapons. \n";
 	cout << sick << " members are sick. \n";
 	cout << "You have " << medicine << " medicine. \n";
+	cout << "You have " << barricades << " barricades. \n";
 }
 
 
