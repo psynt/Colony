4cfd5e8d3514e371d09bc7aa1d63b92d209df05a
nichita <nichita@DeadLee>
2016-10-08 10:44:57 +0100

    skillset

---
+++ src/People.cpp
@@ -0,0 +1,18 @@
+/*
+ * People.cpp
+ *
+ *  Created on: Oct 8, 2016
+ *      Author: nichita
+ */
+
+#include "People.h"
+
+People::People() {
+	// TODO Auto-generated constructor stub
+
+}
+
+People::~People() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/People.h
@@ -0,0 +1,21 @@
+/*
+ * People.h
+ *
+ *  Created on: Oct 8, 2016
+ *      Author: nichita
+ */
+
+#ifndef PEOPLE_H_
+#define PEOPLE_H_
+
+#include "Skillset.h"
+
+
+class People {
+	Skillset s;
+public:
+	People();
+	~People();
+};
+
+#endif /* PEOPLE_H_ */
