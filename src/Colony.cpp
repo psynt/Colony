d0938948811fc22a0fb394bc25d192061926e960
Zane Hartley <zaxa13@gmail.com>
2016-10-08 11:10:43 +0100

    waiting for Nichtia to get his git working

---
+++ src/Colony.cpp
@@ -0,0 +1,17 @@
+/*
+ * Colony.cpp
+ *
+ *  Created on: 8 Oct 2016
+ *      Author: zaxa1
+ */
+
+#include "Colony.h"
+
+Colony::Colony() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Colony::~Colony() {
+	// TODO Auto-generated destructor stub
+}
