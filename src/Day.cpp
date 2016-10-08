d9fbba2e105a60122fc5dfbf96dfa8435d57ba5a
Poh Zhi Ee <poh140996@hotmail.com>
2016-10-08 12:14:07 +0100

    More printstatus updates

--- src/Day.cpp
+++ src/Day.cpp
@@ -11,18 +11,58 @@
 
 using namespace std;
 
-void Day::printStatus(){
-	int member, ration, weapon, sick, medicine;
+void Day::printStatus_init(){
+	int dayNum = Colony.getDay();
 	
-	member = Colony.getPeople();
-	sick   = Colony.getSick();
-	cout << "DAY " << day_num << '\n';								 
+	cout << "DAY " << dayNum << '\n';								 
 	cout << "--------------------------------------------------\n"   
 	cout << "The colony has " << member << " members. \n";			 
-	cout << "You have " << ration << " rations of food. \n";		 
+	cout << "You have " << ration << " ration(s) of food. \n";
+	cout << "You have " << uncooked << " uncooked food. \n"
 	cout << sick << " member is sick. \n";
 	cout << "You have " << medicine << " medicine. \n";
 }
+
+void Day::printStatus_find(){
+	int f_ration;
+	int f_uncooked;
+	int f_weap;
+	int f_med;
+	int barricade;
+	cout << "\nYour search party finds: \n";
+	cout << "\t" << f_ration << " ration(s) of food\n";
+	cout << "\t" << f_uncooked << " uncooked food\n";
+	cout << "\t" << f_weap << " weapon(s)\n";
+	cout << "\t" << f_med << " medicine(s)\n";
+	cout << "The colony now has " << barricade << " barricade(s)\n";
+}
+
+void Day::printStatus_result(){
+	
+	cout << "Night falls.";
+	cout << "You have " << healthy << " healthy members, ";
+	cout << weapon << " weapon(s) and ";
+	cout << barricade << " barricade(s). \n";
+	
+	cout << zombies << " zombies attacked in the night. \n";
+	
+	int res = healthy + weapon + barricade - zombies;
+	if (res==0){
+		cout << "You barely managed to hold them back.\n";
+	}
+	else if(res>0){
+		cout << "Congratulations! You have successfully defended colony from the zombies.\n";
+	}
+	else {
+		Day::printStatus_lose();
+	}
+	
+}
+
+void Day::printStatus_lose(){
+	
+}
+
 
 
 Day::Day() {
