a2935f5869e5f6e121a5b083380d7cb259f9f83e
Poh Zhi Ee <poh140996@hotmail.com>
2016-10-08 13:24:51 +0100

    changed day.cpp
    added more pull variables, added a line for weapon in the initial print

--- src/Day.cpp
+++ src/Day.cpp
@@ -16,27 +16,25 @@ void Day::printStatus_init(){
 	int member=c->getPeople();
 	int ration=c->getRat();
 	int uncooked=c->getUnc();
+	int weapon=c->getWep();
 	int sick=c->getSick();
 	int medicine=c->getMed();
-	/*
-	variables to get
-	current:
-	1) barricade
-	
-	variables are not yet defined but are used in the function, please define
-	*/
+	int barricade=->getBar();
+	//done getting variables
 	
 	cout << "DAY " << dayNum << '\n';								 
 	cout << "--------------------------------------------------\n";
 	cout << "The colony has " << member << " members. \n";			 
 	cout << "You have " << ration << " ration(s) of food. \n";
-	cout << "You have " << uncooked << " uncooked food. \n"
+	cout << "You have " << uncooked << " uncooked food. \n";
+	cout << "You have " << weapon << " weapon(s)\n";
 	cout << sick << " member is sick. \n";
 	cout << "You have " << medicine << " medicine. \n";
 	cout << "You have " << barricade << " barricades. \n";
 }
 
 void Day::printStatus_find(){
+	
 	/*
 	variables to get
 	1) found rations
