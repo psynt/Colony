d2f367e865c96900d0175a066e5b5c6000153839
Samuel Copping <sam.copping@yahoo.com>
2016-10-08 20:36:41 +0100

    God I hope this works

---
+++ src/People.cpp
@@ -0,0 +1,25 @@
+/*
+ * People.cpp
+ *
+ *  Created on: Oct 8, 2016
+ *      Author: nichita
+ */
+
+#include "People.h"
+
+People::People() {
+	// TODO Auto-generated constructor stub
+
+}
+
+People::~People() {
+	// TODO Auto-generated destructor stub
+}
+
+int People::getPeople(){
+	return people;
+}
+int People::getSick(){
+	return sick;
+}
+
