e065095e942c264a11acc09a122e8181fab5016c
sam <sam.copping@yahoo.com>
2016-10-14 22:32:04 +0100

    Added a main menu

--- src/Main.cpp
+++ src/Main.cpp
@@ -9,17 +9,36 @@ int main()
 {
 	srand(time(NULL));
 
-	Colony colony;
-	Day day(&colony);
+	int option;
 
-	while (colony.getPeople() > 0){
-		day.printStatus_init();
-		day.printStatus_find();
-		day.printStatus_result();
-		day.deathRoll();
-		day.EndDay();
-	}
+	do {
+		cout << "========== COLONY ==========\n";
+		cout << "\n";
+		cout << "1. New Game\n";
+		cout << "2. Quit\n";
+		cout << "\n";
+		cout << "OPTION: ";
 
-	cout << "You lasted " << colony.getDay() << " days. Congratulations.\n";
+		cin >> option;
+
+		if (option == 1) {
+			Colony colony;
+			Day day(&colony);
+
+			while (colony.getPeople() > 0){
+				day.printStatus_init();
+				day.printStatus_find();
+				day.printStatus_result();
+				day.deathRoll();
+				day.EndDay();
+			}
+
+			cout << "You lasted " << colony.getDay() << " days. Congratulations.\n\n\n\n";
+		}
+
+	} while (option != 2);
+
+	cout << "\nQuitting...\n";
+
 	return 0;
 }
