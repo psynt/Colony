043bca2e778712ee499422d22a3af846e23dbf90
sam <sam.copping@yahoo.com>
2016-10-14 22:47:07 +0100

    Added "Game" files and class
    With the recent addition of a main menu, this new Game class will hold
    the Colony and Day objects and deal with game implementation, removing
    it from main().

---
+++ src/Game.cpp
@@ -0,0 +1,9 @@
+#include "Game.h"
+
+Game::Game() {
+	// important constructor
+}
+
+Game::~Game() {
+	// important destructor
+}
---
+++ src/Game.h
@@ -0,0 +1,11 @@
+#ifndef SRC_GAME_H_
+#define SRC_GAME_H_
+
+class Game {
+
+public:
+	Game();
+	~Game();
+};
+
+#endif /* SRC_GAME_H_ */
